74aef2de348cd6be14541f9ad53a88e90eae9000
agustina <agustinacec@gmail.com>
2024-09-04 11:32:34 -0300

    Diseño del formulario Login

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace AgMaGest
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new C_Presentacion.LoginForm());
         }
     }
 }
