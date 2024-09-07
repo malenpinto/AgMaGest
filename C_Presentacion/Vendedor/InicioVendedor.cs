ceb96a8d0de76fc4d33714f35e6b6045ec069a2e
malen <malenpinto9@gmail.com>
2024-09-07 15:24:48 -0300

    vista vendedor

---
+++ C_Presentacion/Vendedor/InicioVendedor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgMaGest.C_Presentacion.Vendedor
+{
+    public partial class InicioVendedor : Form
+    {
+        public InicioVendedor()
+        {
+            InitializeComponent();
+        }
+
+        private void BSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
