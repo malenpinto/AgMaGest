a3e22aeb97b9dba49ff5ec6c84730f62f49be7d9
malen <malenpinto9@gmail.com>
2024-09-25 11:48:22 -0300

    creacion vista adm

---
+++ C_Presentacion/Administrador/InicioAdministrador.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgMaGest.C_Presentacion.Administrador
+{
+    public partial class InicioAdministrador : Form
+    {
+        public InicioAdministrador()
+        {
+            InitializeComponent();
+        }
+    }
+}
