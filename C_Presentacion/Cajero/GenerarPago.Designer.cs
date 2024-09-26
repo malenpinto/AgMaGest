8456dad6a979c9edd57516ae00ea1040aa18c93c
agustina <agustinacec@gmail.com>
2024-09-26 12:52:48 -0300

    Prueba de un cambio para extraer y generar commit

--- C_Presentacion/Cajero/GenerarPago.Designer.cs
+++ C_Presentacion/Cajero/GenerarPago.Designer.cs
@@ -29,17 +29,21 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.BSalirGenerarPago = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.BSalirGenerarPago = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.GhostWhite;
+            this.panel1.Controls.Add(this.panel3);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.BSalirGenerarPago);
             this.panel1.Controls.Add(this.panel2);
@@ -47,6 +51,37 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(519, 560);
             this.panel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 2;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCyan;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.button2.Location = new System.Drawing.Point(130, 501);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 41);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Confirmar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // BSalirGenerarPago
+            // 
+            this.BSalirGenerarPago.FlatAppearance.BorderSize = 2;
+            this.BSalirGenerarPago.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.BSalirGenerarPago.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MistyRose;
+            this.BSalirGenerarPago.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BSalirGenerarPago.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BSalirGenerarPago.ForeColor = System.Drawing.Color.Salmon;
+            this.BSalirGenerarPago.Location = new System.Drawing.Point(282, 501);
+            this.BSalirGenerarPago.Name = "BSalirGenerarPago";
+            this.BSalirGenerarPago.Size = new System.Drawing.Size(96, 41);
+            this.BSalirGenerarPago.TabIndex = 1;
+            this.BSalirGenerarPago.Text = "Salir";
+            this.BSalirGenerarPago.UseVisualStyleBackColor = true;
+            this.BSalirGenerarPago.Click += new System.EventHandler(this.BSalirGenerarPago_Click);
             // 
             // panel2
             // 
@@ -68,36 +103,25 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Datos de la Venta";
             // 
-            // BSalirGenerarPago
+            // panel3
             // 
-            this.BSalirGenerarPago.FlatAppearance.BorderSize = 2;
-            this.BSalirGenerarPago.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.BSalirGenerarPago.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MistyRose;
-            this.BSalirGenerarPago.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BSalirGenerarPago.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BSalirGenerarPago.ForeColor = System.Drawing.Color.Salmon;
-            this.BSalirGenerarPago.Location = new System.Drawing.Point(282, 501);
-            this.BSalirGenerarPago.Name = "BSalirGenerarPago";
-            this.BSalirGenerarPago.Size = new System.Drawing.Size(96, 41);
-            this.BSalirGenerarPago.TabIndex = 1;
-            this.BSalirGenerarPago.Text = "Salir";
-            this.BSalirGenerarPago.UseVisualStyleBackColor = true;
-            this.BSalirGenerarPago.Click += new System.EventHandler(this.BSalirGenerarPago_Click);
+            this.panel3.BackColor = System.Drawing.Color.Thistle;
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(24, 269);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(471, 23);
+            this.panel3.TabIndex = 3;
             // 
-            // button2
+            // label2
             // 
-            this.button2.FlatAppearance.BorderSize = 2;
-            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCyan;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.LightSeaGreen;
-            this.button2.Location = new System.Drawing.Point(130, 501);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(103, 41);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Confirmar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.BlueViolet;
+            this.label2.Location = new System.Drawing.Point(184, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 19);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Datos de la Venta";
             // 
             // GenerarPago
             // 
@@ -114,6 +138,8 @@
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -125,5 +151,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button BSalirGenerarPago;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
     }
 }
