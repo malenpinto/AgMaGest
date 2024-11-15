738f2e01edc7259eca4cdd3f9c7ce49a765b2339
malenpinto <malenpinto9@gmail.com>
2024-11-14 23:44:43 -0300

    visualizar pago

--- C_Presentacion/Cajero/RegistrarPago.Designer.cs
+++ C_Presentacion/Cajero/RegistrarPago.Designer.cs
@@ -31,16 +31,17 @@ namespace AgMaGest.C_Presentacion.Cajero
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
             this.panel7 = new System.Windows.Forms.Panel();
-            this.BGenerarVenta = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BBuscarFacturas = new System.Windows.Forms.Button();
+            this.TBBuscarPedido = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.panel6 = new System.Windows.Forms.Panel();
+            this.BGenerarVenta = new System.Windows.Forms.Button();
             this.panel5 = new System.Windows.Forms.Panel();
             this.dataGridPagos = new System.Windows.Forms.DataGridView();
             this.id_Venta = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -53,7 +54,6 @@ namespace AgMaGest.C_Presentacion.Cajero
             this.nombreCompletoCliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.montoTotalVenta = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.estadoVenta = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.BBuscarFacturas = new System.Windows.Forms.Button();
             this.panel4.SuspendLayout();
             this.panel7.SuspendLayout();
             this.panel6.SuspendLayout();
@@ -107,7 +107,7 @@ namespace AgMaGest.C_Presentacion.Cajero
             // 
             this.panel7.BackColor = System.Drawing.Color.GhostWhite;
             this.panel7.Controls.Add(this.BBuscarFacturas);
-            this.panel7.Controls.Add(this.textBox1);
+            this.panel7.Controls.Add(this.TBBuscarPedido);
             this.panel7.Controls.Add(this.label1);
             this.panel7.Location = new System.Drawing.Point(0, 48);
             this.panel7.Margin = new System.Windows.Forms.Padding(4);
@@ -115,25 +115,35 @@ namespace AgMaGest.C_Presentacion.Cajero
             this.panel7.Size = new System.Drawing.Size(598, 57);
             this.panel7.TabIndex = 2;
             // 
-            // BGenerarVenta
+            // BBuscarFacturas
             // 
-            this.BGenerarVenta.Location = new System.Drawing.Point(662, 4);
-            this.BGenerarVenta.Margin = new System.Windows.Forms.Padding(4);
-            this.BGenerarVenta.Name = "BGenerarVenta";
-            this.BGenerarVenta.Size = new System.Drawing.Size(120, 28);
-            this.BGenerarVenta.TabIndex = 3;
-            this.BGenerarVenta.Text = "Generar Pago";
-            this.BGenerarVenta.UseVisualStyleBackColor = true;
-            this.BGenerarVenta.Click += new System.EventHandler(this.BGenerarVenta_Click);
+            this.BBuscarFacturas.BackColor = System.Drawing.Color.Transparent;
+            this.BBuscarFacturas.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BBuscarFacturas.Dock = System.Windows.Forms.DockStyle.Right;
+            this.BBuscarFacturas.FlatAppearance.BorderSize = 0;
+            this.BBuscarFacturas.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.BBuscarFacturas.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.BBuscarFacturas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BBuscarFacturas.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BBuscarFacturas.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(141)))), ((int)(((byte)(25)))));
+            this.BBuscarFacturas.Image = global::AgMaGest.Properties.Resources.Icono_Buscar_Cajero;
+            this.BBuscarFacturas.Location = new System.Drawing.Point(546, 0);
+            this.BBuscarFacturas.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.BBuscarFacturas.Name = "BBuscarFacturas";
+            this.BBuscarFacturas.Size = new System.Drawing.Size(52, 57);
+            this.BBuscarFacturas.TabIndex = 8;
+            this.BBuscarFacturas.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BBuscarFacturas.UseVisualStyleBackColor = false;
+            this.BBuscarFacturas.Click += new System.EventHandler(this.BBuscarFacturas_Click);
             // 
-            // textBox1
+            // TBBuscarPedido
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(287, 11);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(237, 32);
-            this.textBox1.TabIndex = 1;
+            this.TBBuscarPedido.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBBuscarPedido.Location = new System.Drawing.Point(287, 11);
+            this.TBBuscarPedido.Margin = new System.Windows.Forms.Padding(4);
+            this.TBBuscarPedido.Name = "TBBuscarPedido";
+            this.TBBuscarPedido.Size = new System.Drawing.Size(237, 32);
+            this.TBBuscarPedido.TabIndex = 1;
             // 
             // label1
             // 
@@ -157,6 +167,17 @@ namespace AgMaGest.C_Presentacion.Cajero
             this.panel6.Size = new System.Drawing.Size(1030, 48);
             this.panel6.TabIndex = 1;
             // 
+            // BGenerarVenta
+            // 
+            this.BGenerarVenta.Location = new System.Drawing.Point(662, 4);
+            this.BGenerarVenta.Margin = new System.Windows.Forms.Padding(4);
+            this.BGenerarVenta.Name = "BGenerarVenta";
+            this.BGenerarVenta.Size = new System.Drawing.Size(120, 28);
+            this.BGenerarVenta.TabIndex = 3;
+            this.BGenerarVenta.Text = "Generar Pago";
+            this.BGenerarVenta.UseVisualStyleBackColor = true;
+            this.BGenerarVenta.Click += new System.EventHandler(this.BGenerarVenta_Click);
+            // 
             // panel5
             // 
             this.panel5.BackColor = System.Drawing.Color.Thistle;
@@ -169,14 +190,14 @@ namespace AgMaGest.C_Presentacion.Cajero
             // 
             // dataGridPagos
             // 
-            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridPagos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridPagos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridPagos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridPagos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.id_Venta,
@@ -267,27 +288,6 @@ namespace AgMaGest.C_Presentacion.Cajero
             this.estadoVenta.Name = "estadoVenta";
             this.estadoVenta.Width = 125;
             // 
-            // BBuscarFacturas
-            // 
-            this.BBuscarFacturas.BackColor = System.Drawing.Color.Transparent;
-            this.BBuscarFacturas.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.BBuscarFacturas.Dock = System.Windows.Forms.DockStyle.Right;
-            this.BBuscarFacturas.FlatAppearance.BorderSize = 0;
-            this.BBuscarFacturas.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.BBuscarFacturas.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.BBuscarFacturas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BBuscarFacturas.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BBuscarFacturas.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(141)))), ((int)(((byte)(25)))));
-            this.BBuscarFacturas.Image = global::AgMaGest.Properties.Resources.Icono_Buscar_Cajero;
-            this.BBuscarFacturas.Location = new System.Drawing.Point(546, 0);
-            this.BBuscarFacturas.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.BBuscarFacturas.Name = "BBuscarFacturas";
-            this.BBuscarFacturas.Size = new System.Drawing.Size(52, 57);
-            this.BBuscarFacturas.TabIndex = 8;
-            this.BBuscarFacturas.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.BBuscarFacturas.UseVisualStyleBackColor = false;
-            this.BBuscarFacturas.Click += new System.EventHandler(this.BBuscarFacturas_Click);
-            // 
             // RegistrarPago
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -322,7 +322,7 @@ namespace AgMaGest.C_Presentacion.Cajero
         private System.Windows.Forms.DataGridView dataGridPagos;
         private System.Windows.Forms.Panel panel7;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox TBBuscarPedido;
         private System.Windows.Forms.Button BGenerarVenta;
         private System.Windows.Forms.DataGridViewTextBoxColumn id_Venta;
         private System.Windows.Forms.DataGridViewButtonColumn generarPago;
--- C_Presentacion/Cajero/RegistrarPago.cs
+++ C_Presentacion/Cajero/RegistrarPago.cs
@@ -1,9 +1,11 @@
 ﻿using AgMaGest.C_Datos;
 using AgMaGest.C_Logica.Entidades;
+using AgMaGest.C_Presentacion.Administrador;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.IO;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,8 +18,9 @@ namespace AgMaGest.C_Presentacion.Cajero
     {
         public RegistrarPago()
         {
-            InitializeComponent();
             this.Load += new EventHandler(VisualizarVentas_Load);
+            dataGridPagos.CellClick += DataGridPagos_CellClick;
+            dataGridPagos.CellFormatting += DataGridPagos_CellFormatting;
         }
 
         private void VisualizarVentas_Load(object sender, EventArgs e)
@@ -29,16 +32,30 @@ namespace AgMaGest.C_Presentacion.Cajero
         {
             try
             {
-                // Configura el DataGridView y establece AutoGenerateColumns a false
                 ConfigurarDataGridView();
                 dataGridPagos.AutoGenerateColumns = false;
 
-                // Obtener los pedidos desde la base de datos
                 PedidoDAL pedidoDAL = new PedidoDAL();
                 List<Pedido> listaPedidos = pedidoDAL.ObtenerPedidos();
 
                 if (listaPedidos != null && listaPedidos.Count > 0)
                 {
+                    foreach (var pedido in listaPedidos)
+                    {
+                        pedido.DetallesVehiculo = $"{pedido.Vehiculo.CondicionNombre} - {pedido.Vehiculo.Marca} - {pedido.Vehiculo.TipoNombre} - {pedido.Vehiculo.Modelo} - {pedido.Vehiculo.Anio.Year}";
+
+                        // Cargar la imagen del vehículo
+                        pedido.Imagen = null;
+                        if (!string.IsNullOrEmpty(pedido.Vehiculo.RutaImagen) && File.Exists(pedido.Vehiculo.RutaImagen))
+                        {
+                            pedido.Imagen = Image.FromFile(pedido.Vehiculo.RutaImagen);
+                        }
+                        else
+                        {
+                            pedido.Imagen = Properties.Resources.VhiculoPorDefecto; // Imagen predeterminada
+                        }
+                    }
+
                     dataGridPagos.DataSource = new BindingList<Pedido>(listaPedidos);
                 }
                 else
@@ -57,7 +74,9 @@ namespace AgMaGest.C_Presentacion.Cajero
             // Limpiar cualquier configuración previa
             dataGridPagos.Columns.Clear();
 
-            // Configurar las columnas manualmente
+            // Configurar las columnas estándar
+            dataGridPagos.Columns.Add("NombreEstadoPedido", "Estado Pedido");
+            dataGridPagos.Columns["NombreEstadoPedido"].DataPropertyName = "NombreEstadoPedido";
 
             dataGridPagos.Columns.Add("IdPedido", "ID Pedido");
             dataGridPagos.Columns["IdPedido"].DataPropertyName = "IdPedido";
@@ -65,31 +84,49 @@ namespace AgMaGest.C_Presentacion.Cajero
             dataGridPagos.Columns.Add("CUIL", "CUIL Empleado");
             dataGridPagos.Columns["CUIL"].DataPropertyName = "CUIL";
 
-            dataGridPagos.Columns.Add("NombreEmpleado", "Nombre Empleado");
+            dataGridPagos.Columns.Add("NombreEmpleado", "Nombre ");
             dataGridPagos.Columns["NombreEmpleado"].DataPropertyName = "NombreEmpleado";
 
-            dataGridPagos.Columns.Add("ApellidoEmpleado", "Apellido Empleado");
+            dataGridPagos.Columns.Add("ApellidoEmpleado", "Apellido ");
             dataGridPagos.Columns["ApellidoEmpleado"].DataPropertyName = "ApellidoEmpleado";
 
             dataGridPagos.Columns.Add("IdCliente", "ID Cliente");
             dataGridPagos.Columns["IdCliente"].DataPropertyName = "IdCliente";
 
-            dataGridPagos.Columns.Add("IdVehiculo", "ID Vehículo");
-            dataGridPagos.Columns["IdVehiculo"].DataPropertyName = "IdVehiculo";
+            // Columna para los detalles del vehículo
+            DataGridViewImageColumn imageColumn = new DataGridViewImageColumn
+            {
+                Name = "Imagen",
+                HeaderText = "Imagen",
+                ImageLayout = DataGridViewImageCellLayout.Zoom, // Ajusta la imagen al tamaño de la celda
+                Width = 100
+            };
+            dataGridPagos.Columns.Add(imageColumn);
 
+            dataGridPagos.Columns.Add("DetallesVehiculo", "Detalles Vehiculo");
+            dataGridPagos.Columns["DetallesVehiculo"].DataPropertyName = "DetallesVehiculo";
+
+            // Configurar las columnas para fecha y monto
             dataGridPagos.Columns.Add("FechaPedido", "Fecha del Pedido");
             dataGridPagos.Columns["FechaPedido"].DataPropertyName = "FechaPedido";
             dataGridPagos.Columns["FechaPedido"].DefaultCellStyle.Format = "dd/MM/yyyy";
 
             dataGridPagos.Columns.Add("MontoPedido", "Monto Pedido");
             dataGridPagos.Columns["MontoPedido"].DataPropertyName = "MontoPedido";
-            dataGridPagos.Columns["MontoPedido"].DefaultCellStyle.Format = "MontoPedido";
+            dataGridPagos.Columns["MontoPedido"].DefaultCellStyle.Format = "C2";
 
-            dataGridPagos.Columns.Add("IdEstadoPedido", "Estado Pedido");
-            dataGridPagos.Columns["IdEstadoPedido"].DataPropertyName = "IdEstadoPedido";
 
+            dataGridPagos.AllowUserToAddRows = false;
         }
 
+        private void SoloNumeros_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Este campo solo puede contener números.");
+            }
+        }
 
         private void BGenerarVenta_Click(object sender, EventArgs e)
         {
@@ -98,7 +135,7 @@ namespace AgMaGest.C_Presentacion.Cajero
         }
         private void BBuscarFacturas_Click(object sender, EventArgs e)
         {
-            string criterioBusqueda = textBox1.Text.Trim();
+            string criterioBusqueda = TBBuscarPedido.Text.Trim();
 
             if (string.IsNullOrWhiteSpace(criterioBusqueda))
             {
@@ -123,6 +160,81 @@ namespace AgMaGest.C_Presentacion.Cajero
             catch (Exception ex)
             {
                 MessageBox.Show($"Error al buscar los pedidos: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+
+        /*private void DataGridPedidos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dataGridPedidos.Columns["Imagen"].Index && e.RowIndex >= 0)
+            {
+                var imagen = dataGridPedidos.Rows[e.RowIndex].Cells["Imagen"].Value as Image;
+                if (imagen != null)
+                {
+                    VisualizarImagen formImagen = new VisualizarImagen(imagen);
+                    formImagen.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("No hay imagen disponible para este pedido.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }*/
+
+        private void DataGridPagos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Verifica que la columna sea la de la imagen y que la fila sea válida
+            if (e.ColumnIndex == dataGridPagos.Columns["Imagen"].Index && e.RowIndex >= 0)
+            {
+                // Obtiene el objeto Pedido vinculado a la fila seleccionada
+                Pedido pedido = (Pedido)dataGridPagos.Rows[e.RowIndex].DataBoundItem;
+
+                if (pedido?.Vehiculo?.Imagen != null)
+                {
+                    // Muestra la imagen en un formulario de visualización
+                    VisualizarImagen formImagen = new VisualizarImagen(pedido.Vehiculo.Imagen);
+                    formImagen.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("No hay imagen disponible para este pedido.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+
+        private void DataGridPagos_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (dataGridPagos.Columns[e.ColumnIndex].Name == "NombreEstadoPedido")
+            {
+                string estadoPedido = e.Value?.ToString();
+
+                if (!string.IsNullOrEmpty(estadoPedido))
+                {
+                    switch (estadoPedido.ToLower())
+                    {
+                        case "completado":
+                            e.CellStyle.BackColor = Color.LightGreen;
+                            e.CellStyle.ForeColor = Color.Black;
+                            break;
+
+                        case "pendiente":
+                            e.CellStyle.BackColor = Color.LightYellow;
+                            e.CellStyle.ForeColor = Color.Black;
+                            break;
+
+                        case "cancelado":
+                            e.CellStyle.BackColor = Color.LightCoral;
+                            e.CellStyle.ForeColor = Color.White;
+                            break;
+
+                        default:
+                            e.CellStyle.BackColor = Color.White;
+                            e.CellStyle.ForeColor = Color.Black;
+                            break;
+                    }
+                }
             }
         }
     }
